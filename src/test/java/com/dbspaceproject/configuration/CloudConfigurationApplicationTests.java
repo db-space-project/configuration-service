697798dffb34d17f4154655b9f10deea5142c0f9
nirajgeorgian <nirajgeorgian@gmail.com>
2020-10-31 01:33:42 +0530

    initialized application for dbconnection service

---
+++ src/test/java/com/dbspaceproject/configuration/CloudConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbspaceproject.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

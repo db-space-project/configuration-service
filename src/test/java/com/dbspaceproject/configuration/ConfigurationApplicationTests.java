cfbde874742f6e30c26fbaf884d4fb479fd2e163
nirajgeorgian <nirajgeorgian@gmail.com>
2020-11-01 09:43:12 +0530

    changed java version to 11 and added security encrypt option

--- src/test/java/com/dbspaceproject/configuration/ConfigurationApplicationTests.java
+++ src/test/java/com/dbspaceproject/configuration/ConfigurationApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CloudConfigurationApplicationTests {
+class ConfigurationApplicationTests {
 
 	@Test
 	void contextLoads() {
